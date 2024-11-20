82326fe05c7950ee8886a659a86609e63c63bbdc
happenwah <tiagolixa@hotmail.com>
2024-11-20 16:06:10 +0700

    chore: GaslessSwapEntrypoint deployment script

---
+++ scripts/GaslessSwapEntrypointDeploy.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import 'forge-std/Script.sol';
+import { GaslessSwapEntrypoint } from '../src/swap-router/GaslessSwapEntrypoint.sol';
+
+contract GaslessSwapEntrypointDeploy is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint('DEPLOYER_PRIVATE_KEY');
+        address owner = vm.envAddress('GASLESSSWAP_ENTRYPOINT_OWNER');
+        // solhint-disable-next-line var-name-mixedcase
+        address DAI = vm.envAddress('DAI');
+        address swapRouter = vm.envAddress('SWAP_ROUTER');
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        new GaslessSwapEntrypoint(owner, swapRouter, DAI);
+
+        vm.stopBroadcast();
+    }
+}
